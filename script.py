75b35794d7e596ba54d0b093cc10aab160650c4b
yShijin <shingyang997@gmail.com>
2018-10-10 15:18:23 -0400

    Update and rename script to script.py

---
+++ script.py
@@ -0,0 +1,127 @@
+import math
+import numpy
+
+
+# calculate_latlong: given a distance, bearing, and origin (the start coordinate), the function will calculate
+# the new (lat, long)
+# calculate_latlong: Num Num (listof Num) -> (listof Num)
+# the format of origin is [lat, long]
+# return: new [lat, long]
+    
+def calculate_latlong(d, brng, origin):
+    # Radius of the Earth
+    R = 6378.1
+    
+    # get the latitude and longitude
+    lat = math.radians(origin[0])
+    lon = math.radians(origin[1])
+    
+    # calculating new latitude and longitude
+    newlat = math.asin(math.sin(lat)*math.cos(d/R) + \
+             math.cos(lat)*math.sin(d/R)*math.cos(brng))
+
+    newlon = lon + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat),
+                              math.cos(d/R)-math.sin(lat)*math.sin(lat))
+    
+    # convert them back to decimal format
+    newlat = math.degrees(newlat)
+    newlon = math.degrees(newlon)
+    
+    return [newlat, newlon]
+
+
+# extract_info: the function extracts information from legal descriptions/addresses, and conducts different
+# calculation based on the description
+# extract_info: Str -> (listof Num)
+# return: new [lat, long]
+# requires: the script currently can only deal with the format: X#-X#-#X
+    # examples: SW23-19-13W -> south-west quater of the section 23, township 19, range 13, at the west of the first meridian
+    #           NE2-25-23W -> north-east quater of the section 2, township 25, range 23, at the west of the first meridian
+# Currently can only query from the first meridian
+
+def extract_info(legal_description):
+    # lat - the boundary of Canada and United States, long - the prime meridian
+    origin = [49.000649, -97.457889]
+    
+    # location matrix - used to locate the section in a township
+    loc_matrix = np.array([[31,32,33,34,35,36],
+                           [30,29,28,27,26,25],
+                           [19,20,21,22,23,24],
+                           [18,17,16,15,14,13],
+                           [7,8,9,10,11,12],
+                           [6,5,4,3,2,1]], dtype=np.int8)
+    '''
+    list: info_part
+        [0] quater and section
+        [1] township
+        [2] range and direction away from the meridian
+    '''
+    info_part = legal_description.split('-')
+    
+    quater = info_part[0][:2]
+    section = int(info_part[0][2:])
+    township = int(info_part[1])
+    ran = int(info_part[2][0])
+    direction = info_part[2][1]
+    print(quater, section, township, ran, direction)
+    
+    # calculate the distance away from the prime meridian/first meridian, also the distance away from the boundary of Canada
+    # and United States
+    dtownship_mile = township * 6
+    dran_mile = ran * 6
+    print(dtownship_mile, dran_mile)
+    
+    # find section
+    print(loc_matrix)
+    loc = np.where(loc_matrix == section)
+    print(loc)
+    
+    # calculate the distance away from the right-bottom corner of the section 1 in the township
+    to_township = (6 - loc[1].item()) - 1
+    to_range = (6 - loc[0].item()) - 1
+    
+    # convert rate for converting miles to kilometers
+    conv_fac = 1.609344
+    
+    # calculate the distance to get to each quater
+    if quater == "NW":
+        dran_mile += 0.5
+        dtownship_mile -= 0.5
+    elif quater == "NE":
+        dran_mile += 0.5
+    elif quater == "SW":
+        dtownship_mile -= 0.5
+    elif quater == "SE":
+        pass
+    else:
+        print("quater is wrong")
+        return
+    
+    # identify the direction to start from the first meridian, and do the calculation
+    if direction == "W":
+        # add difference in section to range
+        dran_mile += to_range
+        dtownship_mile -= to_township
+        
+        brng = math.atan2(-dran_mile, dtownship_mile)
+        print(math.degrees(brng))
+        to_coord_distance = math.sqrt((dran_mile**2) + (dtownship_mile**2))
+        to_coord_distance = to_coord_distance * conv_fac
+        
+    elif direction == "E":
+        dran_mile += to_range
+        dtownship_mile += to_township
+        
+        brng = math.atan2(dran_mile, dtownship_mile)
+        print(math.degrees(brng))
+        to_coord_distance = math.sqrt((dran_mile**2) + (dtownship_mile**2))
+        to_coord_distance = to_coord_distance * conv_fac
+        
+    else:
+        print('direction is wrong')
+        return
+    
+    print(dtownship_mile, dran_mile)
+    
+    new_coord = calculate_latlong(to_coord_distance, brng, origin)
+    return new_coord
